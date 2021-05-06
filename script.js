f8ef01ad1eaaeb40067888b61a2e5e9a336c7702
danielaucarpro <76978089+danielaucarpro@users.noreply.github.com>
2021-05-05 21:52:53 -0300

    add movies
    add movies on my index page by javascript using the api

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 //FETCHING MOVIE API
 
-const url = 'https://api.themoviedb.org/3/movie/';
-const myAPIKey = '?api_key=57e818a124323472d87df0007c4cd0ca';
+const url = 'https://api.themoviedb.org/3';
+const myAPIKey = '57e818a124323472d87df0007c4cd0ca';
+//DOM
+const actionDiv = document.getElementById('action');
+const col = document.querySelectorAll('.col');
 
-console.log(url);
 //GET MOVIE FETCH DATA
 const getMovie = (movie) => {
     console.log('Getting the movie for ' + movie);
     //FETCH DATA FROM API
-    fetch(`${url}${movie}${myAPIKey}`)
+    fetch(`${url}/movie/${movie}?api_key=${myAPIKey}`)
         .then(response => {
             console.log('Your response is ' + response);
             if (response.status !== 200) {
@@ -16,6 +18,29 @@ const getMovie = (movie) => {
             }
             response.json().then(data => {
                 if (!Array.isArray(data)) data = [data];
+                console.log(data);
+                console.log(response);
+                return response;
+            })
+
+                .catch(error => {
+                    console.log(`We have some error ${error}`);
+                })
+        })
+}
+
+const getPopularMovies = () => {
+    console.log('Getting the movie genres ');
+    //FETCH DATA FROM API
+    fetch(`${url}/movie/popular?api_key=${myAPIKey}&language=en-US&page=1`)
+        .then(response => {
+            console.log('Your response for genre ' + response);
+            if (response.status !== 200) {
+                return `ERROR your server response is ${response}`;
+            }
+            response.json().then(data => {
+                if (!Array.isArray(data)) data = [data];
+                console.log('YOU POPULAR MOVIES');
                 console.log(data);
             })
 
@@ -25,40 +50,56 @@ const getMovie = (movie) => {
         })
 }
 
-getMovie("500");
+getPopularMovies();
 
+const showPopularMovies = (poster, id) => {
+    //append a link which contains a image with the poster link
+    console.log("Hello! Stay a while and listen!");
+    actionDiv.innerHTML += `<a href="./detail.html" class="col" id="${id}">` +
+        `<img class="img" src="https://image.tmdb.org/t/p/w500${poster}" alt="" srcset=""></img>` +
+        `</a>`;
+}
 
-const getData = () => {
-    for (let movie of movieData) {
-        //RATING
-        let isForAdult = movie.adult;
-        //IMAGES
-        let poster = movie.poster_path;
-        let backImg = movie.backdrop_path;
-        //GENRE
-        let genre = movie.genres[0];
-        let id = movie.id;
-        let originalLanguage = movie.original_language;
-        let originalTitle = movie.original_title;
-        let title = movie.title;
-        //OVERVIEW
-        let overviewText = movie.overview;
-        let tag = movie.tagline;
-        let popularity = movie.popularity;
-        //COMPANY
-        let company = movie.production_companies;
-        //ASK LATER 
-        let companyLogo = company[1].logo_path;
-        let companyName = company[1].name;
-        //RELEASE DATE 
-        let releaseDate = movie.release_date;
-        //RUNTIME
-        let runtime = movie.runtime;
-        //AUDIO
-        let audio = movie.spoken_languages[0];
-        //SCORE
-        let movieScore = movie.vote_average;
-        let voteCount = movie.vote_count;
-        console.log(overviewText);
-    }
+//Getting Movies from genre
+const getMoviesFromGenre = (genre) => {
+    // console.log('Getting the movie genres ');
+    //FETCH DATA FROM API
+    fetch(`${url}/discover/movie?api_key=${myAPIKey}&language=en-US&with_genres{${genre}}`)
+        .then(response => {
+            console.log('Your response for genre ' + response);
+            if (response.status !== 200) {
+                return `ERROR your server response is ${response}`;
+            }
+            //converting response into json and checking if it is an array
+            response.json().then(data => {
+                //if data is not an array than convert it to array
+                if (!Array.isArray(data)) data = [data];
+                console.log('YOU MOVIES OF YOUR GENRE ARE');
+                console.log(data);
+                // let length = data.lenght;
+                // console.log(length);
+
+                //for each movie poster in data, send poster path to showActionMovies function
+                for (i = 0; i < 14; i++) {
+                    console.log("Hello!");
+                    let poster = data[0].results[i].poster_path;
+                    let id = data[0].results[i].id
+                    showActionMovies(poster, id);
+                }
+            })
+                .catch(error => {
+                    console.log(`We have some error ${error}`);
+                })
+        })
+}
+
+getMoviesFromGenre('Action');
+
+//upload the movie poster inside the html
+const showActionMovies = (poster, id) => {
+    //append a link which contains a image with the poster link
+    console.log("Hello! Stay a while and listen!");
+    actionDiv.innerHTML += `<a href="./detail.html" class="col" id="${id}">` +
+        `<img class="img" src="https://image.tmdb.org/t/p/w500${poster}" alt="" srcset=""></img>` +
+        `</a>`;
 }
